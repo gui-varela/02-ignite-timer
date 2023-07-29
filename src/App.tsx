78817eb759d5f0d961ebe01c5e9818b0b9a4b8f1
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-07-28 23:42:43 -0300

    Aula 2.5.1 - Entendendo contextos
    O componente Home foi criado nesta aula apenas para fins didáticos. Ele será deletado no próximo commit e então prosseguiremos com os commits de aula.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Home } from './Home'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-        <GlobalStyle />
-      </BrowserRouter>
-    </ThemeProvider>
-  )
+  return <Home />
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //       <GlobalStyle />
+  //     </BrowserRouter>
+  //   </ThemeProvider>
+  // )
 }
