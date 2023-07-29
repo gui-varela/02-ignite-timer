34352063bfed5c273f132e47567990e87056f946
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-07-28 23:43:48 -0300

    Deletando componente Home para dar prosseguimento às aulas

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { Home } from './Home'
 
 export function App() {
-  return <Home />
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //       <GlobalStyle />
-  //     </BrowserRouter>
-  //   </ThemeProvider>
-  // )
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
